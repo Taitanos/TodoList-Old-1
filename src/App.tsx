afe46d4f601d8c7a5ec9ee3be8561eca9b2278a8
Taitanos <hayaashi87@gmail.com>
2021-01-26 22:43:48 +0300

    structuring, props

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,30 @@ import React from 'react';
 import './App.css';
 import TodoList from "./TodoList";
 
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
 function App() {
+    const tasks1: Array<TaskType> = [
+        {id:1, title:"JS", isDone: true},
+        {id:2, title:"CSS", isDone: true},
+        {id:3, title:"React", isDone: false}
+
+    ]
+    const tasks2: Array<TaskType> = [
+        {id:1, title:"HTML", isDone: true},
+        {id:2, title:"Books", isDone: false},
+        {id:3, title:"Github", isDone: true}
+
+    ]
+
     return (
         <div className="App">
-            <TodoList />
+            <TodoList title={"What to learn"} tasks={tasks1}/>
+            <TodoList title={"What to read"} tasks={tasks2}/>
         </div>
     );
 }
