0828e2f002eb06bdafb3f0781b4df30d6bb48f2c
Taitanos <hayaashi87@gmail.com>
2021-03-08 12:06:58 +0300

    add props for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,15 @@ function App() {
     const tasks1: Array<TaskType> = [
         {id:1, title:"JS", isDone: true},
         {id:2, title:"CSS", isDone: true},
-        {id:3, title:"React", isDone: false}
-
-    ]
-    const tasks2: Array<TaskType> = [
-        {id:1, title:"HTML", isDone: true},
-        {id:2, title:"Books", isDone: false},
-        {id:3, title:"Github", isDone: true}
+        {id:3, title:"React", isDone: false},
+        {id:4, title:"Rest API", isDone: false},
+        {id:5, title:"GraphQL", isDone: false}
 
     ]
 
     return (
         <div className="App">
             <TodoList title={"What to learn"} tasks={tasks1}/>
-            <TodoList title={"What to read"} tasks={tasks2}/>
         </div>
     );
 }
